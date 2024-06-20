da75ac40a1e9e8e1b9464cec2ae0f11d8c74dc3e
Daniel Guzman Sagredo <danguz80@outlook.com>
2024-06-20 06:34:26 -0400

    Cargando imagenes de forma local

--- assets/js/venta.js
+++ assets/js/venta.js
@@ -1,7 +1,7 @@
 const propiedades_venta = [
     {
         nombre: 'Apartamento de lujo en zona exclusiva',
-        src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+        src: './assets/img/apartamento_lujo_venta.jpg',
         descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
         ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
         habitaciones: "4 Habitaciones | ",
@@ -12,7 +12,7 @@ const propiedades_venta = [
     },
     {
         nombre: 'Apartamento acogedor en la montaña',
-        src: 'https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg',
+        src: './assets/img/casa_rodante_venta.webp',
         descripcion: 'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
         ubicacion: '789 Mountain Road, Summit Peaks, CA 23456',
         habitaciones: "2 Habitaciones | ",
@@ -23,7 +23,7 @@ const propiedades_venta = [
     },
     {
         nombre: 'Penthouse de lujo con terraza panorámica',
-        src: '"https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg"',
+        src: './assets/img/penthouse_venta.avif',
         descripcion: 'Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares',
         ubicacion: '567 Skyline Avenue, Skyview City, CA 56789',
         habitaciones: "3 Habitaciones | ",
